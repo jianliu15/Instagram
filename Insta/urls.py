8de6d93a4c6806fab2c4e10cc230d74bb3b28573
dugujian1984 <dugujian1984@gmail.com>
2020-01-01 16:19:46 -0500

    Implement CRUD

--- Insta/urls.py
+++ Insta/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from Insta.views import HelloWorld
+from Insta.views import (HelloWorld, PostListView, PostDetailView, 
+                        PostCreateView, PostUpdateView, PostDeleteView)
 
 urlpatterns = [
-    path('', HelloWorld.as_view(), name='helloworld'),
+    path('helloworld/', HelloWorld.as_view(), name='helloworld'),
+    path('', PostListView.as_view(), name='posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/create/', PostCreateView.as_view(), name='post_create'),
+    path('post/update/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
 ]
